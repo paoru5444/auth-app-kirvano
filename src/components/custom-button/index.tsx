698ab3b41e2cc6aac0745c5bc98a50b4afb2c7d8
paoru5444 <betinho5444@gmail.com>
2026-01-12 00:19:55 -0300

    add is submitting indicator to custom button

--- src/components/custom-button/index.tsx
+++ src/components/custom-button/index.tsx
@@ -1,6 +1,6 @@
 import { ButtonVariantTypes } from '@/shared/types/components.types';
 import React from 'react';
-import { Image, ImageSourcePropType } from 'react-native';
+import { ActivityIndicator, Image, ImageSourcePropType } from 'react-native';
 import Typography from '../typography';
 import { ButtonWrapper, variants } from './styles';
 
@@ -9,6 +9,7 @@ interface CustomButtonProps {
   variant?: ButtonVariantTypes;
   leftIcon?: ImageSourcePropType;
   onPress: () => void;
+  isSubmitting?: boolean
 }
 
 export default function CustomButton({
@@ -16,15 +17,16 @@ export default function CustomButton({
   variant = 'primary',
   leftIcon,
   onPress,
+  isSubmitting,
   ...props
 }: CustomButtonProps) {
   return (
-    <ButtonWrapper variant={variant} onPress={onPress} {...props}>
+    <ButtonWrapper variant={variant} onPress={onPress} disabled={isSubmitting} {...props}>
       {leftIcon && (
         <Image source={leftIcon} style={{ width: 18, height: 18 }} />
       )}
       <Typography size='14px' weight={700} color={variants[variant].textColor}>
-        {label}
+        {isSubmitting ? <ActivityIndicator color={'#fff'} /> : label}
       </Typography>
     </ButtonWrapper>
   )
